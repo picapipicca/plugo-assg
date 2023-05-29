2f1bae0c9381cd53cc24b43f2c56e9ee15827f19
picapipicca <haewon09106@gmail.com>
2023-05-30 02:31:45 +0900

    new:Item create & upload

--- src/pages/Admin.tsx
+++ src/pages/Admin.tsx
@@ -1,11 +1,178 @@
-import React from 'react';
-
+import React, { useEffect, useState } from "react";
+import { useMutation } from "react-query";
+import { useNavigate } from "react-router-dom";
+import { useNewItemStore } from "../zustand/store";
+interface newItemProps {
+  title: string;
+  price: number;
+  description: string;
+  itemPreview?: string;
+}
 const Admin = () => {
-    return (
+  const navigate = useNavigate();
+  const [title, setTitle] = useState("");
+  const [price, setPrice] = useState(0);
+  const [description, setDescription] = useState("");
+  const [itemPhoto, setItemPhoto] = useState<FileList | null>();
+  const [itemPreview, setItemPreview] = useState("");
+
+  const uploadItemQuery = async ({
+    title,
+    price,
+    description,
+    itemPreview,
+  }: newItemProps) => {
+    const data = await fetch("https://dummyjson.com/products/add", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        title,
+        price,
+        description,
+        itemPreview,
+      }),
+    }).then((res) => res.json());
+
+    return data;
+  };
+
+  const { setItem } = useNewItemStore();
+  const uploadItem = useMutation(uploadItemQuery, {
+    onSuccess: (data) => {
+      setItem({
+        id: data.id,
+        title: data.title,
+        price: data.price,
+        description: data.description,
+        itemPhoto: data.itemPreview,
+      });
+      navigate(`/item/${data.id}`);
+    },
+  });
+  useEffect(() => {
+    if (itemPhoto && itemPhoto.length > 0) {
+      const file = itemPhoto[0];
+      setItemPreview(URL.createObjectURL(file));
+    } else if (itemPhoto === null) {
+      setItemPreview("");
+    }
+  }, [itemPhoto]);
+
+  return (
+    <>
+      <h2 className="text-3xl font-bold">Upload New Product</h2>
+      <form className={" space-y-5 pt-10"}>
         <div>
-            <h2>Admin</h2>
+          {itemPreview ? (
+            <div>
+              <img
+                src={itemPreview}
+                alt="img preview"
+                className={"w-full h-48 rounded-md mb-3 text-gray-600 "}
+              />
+              <div className="w-full flex justify-end">
+                <button
+                  className="cursor-pointer mb-3 w-8 h-8 flex justify-center hover:bg-gray-200 hover:rounded-full"
+                  onClick={() => setItemPhoto(null)}
+                >
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    strokeWidth={1.5}
+                    stroke="currentColor"
+                    className="w-6 h-6 mt-1"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0"
+                    />
+                  </svg>
+                </button>
+              </div>
+            </div>
+          ) : (
+            <label
+              className={
+                "cursor-pointer w-full flex items-center justify-center border-2 border-dashed border-gray-300 h-48 rounded-md mb-3 text-gray-600 hover:text-blue-500 hover:border-blue-500"
+              }
+            >
+              <svg
+                className="h-12 w-12"
+                stroke="currentColor"
+                fill="none"
+                viewBox="0 0 48 48"
+                aria-hidden="true"
+              >
+                <path
+                  d="M28 8H12a4 4 0 00-4 4v20m32-12v8m0 0v8a4 4 0 01-4 4H12a4 4 0 01-4-4v-4m32-4l-3.172-3.172a4 4 0 00-5.656 0L28 28M8 32l9.172-9.172a4 4 0 015.656 0L28 28m0 0l4 4m4-24h8m-4-4v8m-12 4h.02"
+                  strokeWidth={2}
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                />
+              </svg>
+              <input
+                type="file"
+                accept="image/*"
+                className={"hidden"}
+                onChange={(e) => setItemPhoto(e.target.files)}
+              />
+            </label>
+          )}
         </div>
-    );
+
+        <div className="space-y-4">
+          <label
+            htmlFor={"title"}
+            className="mb-1.5 block text-sm font-medium text-gray-700"
+          >
+            Title
+          </label>
+          <input
+            onChange={(e) => setTitle(e.target.value)}
+            id="title"
+            required
+            className="px-3 appearance-none w-full py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-blue-500 focus:border-blue-500"
+          />
+          <label
+            htmlFor={"price"}
+            className="mb-1.5 block text-sm font-medium text-gray-700"
+          >
+            Price
+          </label>
+          <input
+            onChange={(e) => setPrice(Number(e.target.value))}
+            id="price"
+            required
+            type="number"
+            placeholder="0"
+            className="px-3 appearance-none w-full py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-blue-500 focus:border-blue-500"
+          />
+          <label
+            htmlFor={"description"}
+            className="mb-1.5 block text-sm font-medium text-gray-700"
+          >
+            Description
+          </label>
+          <textarea
+            onChange={(e) => setDescription(e.target.value)}
+            id="description"
+            required
+            className="px-3 appearance-none w-full py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-blue-500 focus:border-blue-500"
+          />
+        </div>
+      </form>
+      <button
+        className="mt-4 hover:bg-blue-700 w-full bg-blue-500 text-white py-2 rounded-md"
+        onClick={() => {
+          uploadItem.mutate({ title, price, description, itemPreview });
+        }}
+      >
+        {uploadItem.isLoading ? "...Loading" : "Upload"}
+      </button>
+    </>
+  );
 };
 
 export default Admin;
--- src/pages/item/ItemDetail.tsx
+++ src/pages/item/ItemDetail.tsx
@@ -1,31 +1,43 @@
 import React, { useState } from "react";
 import { useQuery } from "react-query";
-import { useParams } from "react-router-dom";
-import useCartStore, { ItemSingleState } from "../../zustand/store";
+import { useNavigate, useParams } from "react-router-dom";
+import {
+  useCartStore,
+  ItemSingleState,
+  useNewItemStore,
+} from "../../zustand/store";
 
 interface ItemDetailProps {
   id: number;
   title: string;
   description: string;
   price: number;
-  discountPercentage: number;
-  rating: number;
-  stock: number;
-  brand: string;
-  category: string;
-  thumbnail: string;
-  images: string[];
+  discountPercentage?: number;
+  rating?: number;
+  stock?: number;
+  brand?: string;
+  category?: string;
+  thumbnail?: string;
+  images?: string[];
 }
 
 const ItemDetail = () => {
+  const navigate = useNavigate();
   const { id } = useParams();
   const [currentImg, setCurrentImg] = useState<number>(0);
   const [selected, setSelected] = useState<string>("");
-  const { data, isLoading } = useQuery<ItemDetailProps>("singleItem", () =>
-    fetch(`https://dummyjson.com/products/${id}`).then((res) => res.json())
-  );
-  const { items, addItem } = useCartStore();
+  const { data, isLoading } = useQuery<ItemDetailProps>({
+    queryKey: "singleItem",
+    queryFn: async () => {
+      const response = await fetch(`https://dummyjson.com/products/${id}`);
+      if (response.status === 404) {
+        return null;
+      } else return response.json();
+    },
+  });
 
+  const { addItem } = useCartStore();
+  const { uploadItem } = useNewItemStore();
   const addCartItem = () => {
     const item: ItemSingleState = {
       id: Number(id),
@@ -41,85 +53,128 @@ const ItemDetail = () => {
     setCurrentImg(idx);
   };
 
-  if (isLoading) return <h2>Loading...</h2>;
-  console.log("::::::::items::::::", items);
-
+  if (isLoading) return <h2 className="text-3xl">Loading...</h2>;
+  if (!data && !uploadItem) {
+    navigate("/item");
+  }
+  
   return (
-    <div>
-      <div className={"px-4 py-4"}>
-        <div className={"mb-8 flex space-x-8"}>
-          <div className="relative w-1/2 flex space-x-2">
-            <ul className="w-12">
-              {data?.images.map((img, idx) => {
-                return (
-                  <div
-                    key={idx}
-                    className="aspect-square mb-2 cursor-pointer"
-                    onMouseOver={() => changeImg(idx)}
-                  >
-                    <img
-                      alt="상품이미지"
-                      src={img}
-                      className="object-cover h-full hover:rounded-lg"
-                    />
-                  </div>
-                );
-              })}
-            </ul>
-            <div className="w-4/5">
-              <img
-                src={data?.images[currentImg]}
-                className={"w-full h-full rounded-lg object-cover"}
-                alt="product-thumnail"
-              />
+    <>
+      {data ? (
+        <div className={"px-4 py-4"}>
+          <div className={"mb-8 flex space-x-8"}>
+            <div className="relative w-1/2 flex space-x-2">
+              <ul className="w-1">
+                {data?.images &&
+                  data?.images.map((img, idx) => {
+                    return (
+                      <div
+                        key={idx}
+                        className="aspect-square mb-2 cursor-pointer"
+                        onMouseOver={() => changeImg(idx)}
+                      >
+                        <img
+                          alt="product"
+                          src={img}
+                          className="object-cover h-full hover:rounded-lg"
+                        />
+                      </div>
+                    );
+                  })}
+              </ul>
+              <div className="w-4/5">
+                <img
+                  src={data?.images && data?.images[currentImg]}
+                  className={"w-full h-full rounded-lg object-cover"}
+                  alt="product-thumnail"
+                />
+              </div>
             </div>
-          </div>
 
-          <div className={"mt-5 w-1/2"}>
-            <div className="flex justify-between text-center">
-              <h1 className={"text-3xl font-bold text-gray-900"}>
-                {data?.brand}
-              </h1>
-              <p className="inline-block w-fit px-2 bg-black text-white leading-8">
-                {data?.stock} in stock
+            <div className={"mt-5 w-1/2"}>
+              <div className="flex justify-between text-center">
+                <h1 className={"text-3xl font-bold text-gray-900"}>
+                  {data?.brand}
+                </h1>
+                <p className="inline-block w-fit px-2 bg-black text-white leading-8">
+                  {data?.stock} in stock
+                </p>
+              </div>
+              <p className={"text-3xl mt-3 text-gray-700 block"}>
+                {data?.title}
               </p>
-            </div>
-            <p className={"text-3xl mt-3 text-gray-700 block"}>{data?.title}</p>
-            <p className={"text-3xl mt-3 text-gray-500 block"}>
-              Price {data?.price}
-            </p>
-            <p className={"text-base my-6 text-gray-700"}>
-              {data?.description}
-            </p>
-            <div className="w-full mb-4">
-              <select
-                className="w-full h-10 bg-sky-50 border border-gray-100 px-2"
-                onChange={(e) => setSelected(e.target.value)}
-                value={selected}
-              >
-                {[1, 2, 3, 4, 5].map((num) => {
-                  return (
-                    <option key={num} value={num}>
-                      {num}
-                    </option>
-                  );
-                })}
-              </select>
-            </div>
-            <div className={"flex items-center justify-between space-x-2"}>
-              <button
-                onClick={addCartItem}
-                className={
-                  "flex-1 bg-gray-600 text-white py-3 rounded-md font-medium focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gary-500 hover:bg-gray-700"
-                }
-              >
-                Cart
-              </button>
+              <p className={"text-3xl mt-3 text-gray-500 block"}>
+                Price {data?.price}
+              </p>
+              <p className={"text-base my-6 text-gray-700"}>
+                {data?.description}
+              </p>
+              <div className="w-full mb-4">
+                <select
+                  className="w-full h-10 bg-sky-50 border border-gray-100 px-2"
+                  onChange={(e) => setSelected(e.target.value)}
+                  value={selected}
+                >
+                  {[1, 2, 3, 4, 5].map((num) => {
+                    return (
+                      <option key={num} value={num}>
+                        {num}
+                      </option>
+                    );
+                  })}
+                </select>
+              </div>
+              <div className={"flex items-center justify-between space-x-2"}>
+                <button
+                  onClick={addCartItem}
+                  className={
+                    "flex-1 bg-gray-600 text-white py-3 rounded-md font-medium focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gary-500 hover:bg-gray-700"
+                  }
+                >
+                  Cart
+                </button>
+              </div>
             </div>
           </div>
         </div>
-      </div>
-    </div>
+      ) : (
+        <div className={"px-4 py-4"}>
+          <div className={"mb-8 flex space-x-8"}>
+            <div className="relative w-1/2 flex space-x-2">
+              <div className="w-full">
+                {uploadItem.itemPhoto === "" || undefined  || !uploadItem.itemPhoto? (
+                  <div
+                    className={
+                      "mx-auto text-center flex w-full h-full rounded-lg bg-gray-300"
+                    }
+                  >
+                    <p className="my-auto mx-auto text-3xl font-bole">No Image</p>
+                  </div>
+                ) : (
+                  <img
+                    src={uploadItem.itemPhoto}
+                    className={"w-full h-full rounded-lg object-cover"}
+                    alt="product-thumnail"
+                  />
+                )}
+              </div>
+            </div>
+            <div className={"mt-5 w-1/2"}>
+              <div className="flex justify-between text-center"></div>
+              <p className={"text-3xl mt-3 text-gray-700 block"}>
+                {uploadItem?.title}
+              </p>
+              <p className={"text-xl mt-3 text-gray-500 block"}>
+                Price {uploadItem?.price}
+              </p>
+              <p className={"text-base my-6 text-gray-700"}>
+                {uploadItem?.description}
+              </p>
+            </div>
+          </div>
+        </div>
+      )}
+    </>
   );
 };
 
