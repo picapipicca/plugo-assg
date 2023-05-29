e0329616841ff73b141a8b05b42aaa25a155601b
picapipicca <haewon09106@gmail.com>
2023-05-30 02:30:48 +0900

    modify:css

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -4,10 +4,10 @@ import { Outlet } from "react-router-dom";
 
 const Layout = () => {
   return (
-    <div className="min-h-screen border border-blue-500">
+    <div className="min-h-screen">
       <div>
         <Header />
-        <main className="py-4 px-6 border-2 flex flex-col border-emerald-500">
+        <main className="py-4 px-6">
           <Outlet />
         </main>
       </div>
