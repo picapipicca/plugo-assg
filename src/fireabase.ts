36d803c2304f101c954e1fcf491ddb5b675c53c3
picapipicca <haewon09106@gmail.com>
2023-05-30 13:43:35 +0900

    new:firebase settings

---
+++ src/fireabase.ts
@@ -0,0 +1,14 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCAvLNds70PXXFRfy-UZ2pwwrgFEuMdKHQ",
+  authDomain: "plugoassignment.firebaseapp.com",
+  projectId: "plugoassignment",
+  storageBucket: "plugoassignment.appspot.com",
+  messagingSenderId: "419671551750",
+  appId: "1:419671551750:web:762b7a06aa79ea890fb887",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
